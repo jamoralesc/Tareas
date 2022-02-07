0127a6bee19aec43f7ced78376dffbc2cbf18e1f
Jose Morales <moralescarrera17@gmail.com>
2022-02-06 20:34:53 -0600

    first commit

---
+++ Ejercicio1.js
@@ -0,0 +1,17 @@
+var mes = 10
+
+if (mes > 2 && mes < 6) {
+  alert("es primavera")
+}
+else if (mes > 5 && mes < 9){
+  alert("es verano")
+}
+else if (mes > 8 && mes < 12){
+  alert("es otoño")
+}
+
+else{
+  alert("es invierno")
+}
+
+alert("Carnet 10182190 - Jose Armando Morales Carrera")
